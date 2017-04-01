1134549e2db41698bcf41a45638e8be57ff083e5
TomokazuHiguchi <t.higuchi.robotech@gmail.com>
2017-04-01 15:42:06 +0900

    右側に文字を出力できるように変更

--- app/src/main/java/aawxc/bdm/MainActivity.java
+++ app/src/main/java/aawxc/bdm/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private CameraPreview mPreview;
@@ -13,14 +14,17 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.v("hoge", "piyo");
 
         //don't show navigation bar
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
             View decorView = this.getWindow().getDecorView();
             decorView.setSystemUiVisibility( View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
+
         setContentView(R.layout.activity_main);
+
+        TextView stateView = (TextView) findViewById(R.id.state_view);
+        stateView.setText("Hello");
     }
 
     @Override
