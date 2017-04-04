4a89542d96ed0353444885fb8195d33e729c6ebc
TomokazuHiguchi <t.higuchi.robotech@gmail.com>
2017-04-05 00:21:49 +0900

    GLSurfaceViewの内容をCameraPreviewの上に透過で描画できるようにした

--- app/src/main/java/aawxc/bdm/MainActivity.java
+++ app/src/main/java/aawxc/bdm/MainActivity.java
@@ -1,21 +1,29 @@
 package aawxc.bdm;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorEvent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements SensorEventListener{
 
+    private MyGLSurfaceView mMyGLSurfaceView;
+    private GestureDetector mGestureDetector = null;
+
     private CameraPreview mPreview;
     private SensorManager mSensorManager;
     private TextView stateView;
+
+    private Context mContext;
 
     private float axisX;
     private float axisY;
@@ -24,6 +32,7 @@ public class MainActivity extends Activity implements SensorEventListener{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = getApplicationContext();
 
         //don't show navigation bar
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
@@ -37,11 +46,23 @@ public class MainActivity extends Activity implements SensorEventListener{
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         stateView = (TextView) findViewById(R.id.state_view);
+
+    }
+
+    @Override
+    public  boolean onTouchEvent(MotionEvent event){
+        return mGestureDetector.onTouchEvent(event);
     }
 
     @Override
     public void onResume(){
         super.onResume();
+
+        mMyGLSurfaceView = new MyGLSurfaceView(mContext);
+        FrameLayout glsurfaceview = (FrameLayout) findViewById(R.id.opengl_view);
+        glsurfaceview.addView(mMyGLSurfaceView);
+        mGestureDetector = new GestureDetector(this, (GestureDetector.OnGestureListener)mMyGLSurfaceView);
+
 
         mPreview = new CameraPreview(this);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
@@ -50,6 +71,8 @@ public class MainActivity extends Activity implements SensorEventListener{
         // Listenerの登録
         Sensor accel = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
         mSensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+
+        mMyGLSurfaceView.onResume();
     }
 
     @Override
@@ -57,6 +80,8 @@ public class MainActivity extends Activity implements SensorEventListener{
         super.onPause();
         //Listenerを解除
         mSensorManager.unregisterListener(this);
+
+        mMyGLSurfaceView.onPause();
     }
 
     @Override
---
+++ app/src/main/java/aawxc/bdm/MyGLSurfaceView.java
@@ -0,0 +1,57 @@
+package aawxc.bdm;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+/**
+ * Created by aawxc on 2017/04/03.
+ */
+
+public class MyGLSurfaceView extends GLSurfaceView implements GestureDetector.OnGestureListener {
+
+    private final Context mContext;
+    private GLRenderer renderer;
+
+    public MyGLSurfaceView(Context context){
+        super(context);
+        mContext = context;
+        setEGLContextClientVersion(2);
+        setZOrderOnTop(true);
+        setEGLConfigChooser(8, 8, 8, 8, 0, 0);
+        this.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        renderer = new GLRenderer(context);
+        setRenderer(renderer);
+    }
+
+    @Override
+    public boolean onFling(MotionEvent arg0, MotionEvent arg1, float arg2, float arg3) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent event1, MotionEvent event2, float distx, float disty) {
+        renderer.setScrollValue(distx, disty);
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent arg0) {
+    }
+    @Override
+
+    public void onShowPress(MotionEvent arg0) {
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent arg0) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent arg0) {
+        return false;
+    }
+}
